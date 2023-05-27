c7ceeef5f5a36ba4d82b5c0788ba1fa94ce51aca
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-05-27 18:57:50 -0500

    re-enables remote SP

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
 //		.package(path: "../feather-core"),
-        .package(url: "https://github.com/feathercms/feather-core", .branch("test-refactored-modules")),
+        .package(url: "https://github.com/Rando-Coderissian/feather-core", .branch("test-refactored-modules")),
 		
 //        .package(url: "https://github.com/feathercms/feather-core", .branch("dev")),
         .package(url: "https://github.com/JohnSundell/Ink", from: "0.5.0"),
